e59e747a90e49326f54d8d06eb742602ce4ce056
sandeep kumar sahoo <java.sandeep007@gmail.com>
2018-12-10 16:12:32 +0530

    Update RasaController.java

--- src/main/java/com/rasa/demo/RasaController.java
+++ src/main/java/com/rasa/demo/RasaController.java
@@ -7,22 +7,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class RasaController {
 
+	
+	@Value("${RASA_CORE_WEBHOOK_URL}")
+	private String URL; 
+	
 	@RequestMapping("/core/message/{message}")
 	public String get(@PathVariable String message) {
 
 		RestTemplate restTemplate = new RestTemplate();
 
-		String url = "http://localhost:5005/webhooks/rest/webhook";
+		//String url = "http://localhost:5005/webhooks/rest/webhook";
 		String requestJson = "{\"message\":\""+message+"\"}";
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 
 		HttpEntity<String> entity = new HttpEntity<String>(requestJson, headers);
-		String answer = restTemplate.postForObject(url, entity, String.class);
+		String answer = restTemplate.postForObject(URL, entity, String.class);
 		System.out.println(answer);
 
 		return answer;
